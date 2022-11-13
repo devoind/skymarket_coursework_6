3617437c39f19419374a9f6fef60bca296cc4c8d
Deadpo_Ol <https://github.com/devoind>
2022-11-13 17:15:06 +0300

    Second_job_cw6

---
+++ skymarket/users/managers.py
@@ -0,0 +1,49 @@
+# Менеджер должен быть унаследован от следующего класса
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+
+
+class UserRoles(models.TextChoices):
+    USER = 'user'
+    ADMIN = 'admin'
+
+
+# Менеджер должен содержать как минимум две следующие функции
+class UserManager(BaseUserManager):
+    """
+    Функция создания пользователя — в нее мы передаем обязательные поля
+    """
+
+    def create_user(self, email, first_name, last_name, phone, role=UserRoles.USER, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            role=role,
+        )
+        user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
+        """
+        Функция для создания суперпользователя — с ее помощью мы создаем админинстратора
+        это можно сделать с помощью команды createsuperuser
+        """
+
+        user = self.create_user(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            password=password,
+            role=UserRoles.ADMIN
+        )
+
+        user.save(using=self._db)
+        return user
