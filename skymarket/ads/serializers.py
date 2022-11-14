1feb163feaace717dbd9a9f0d958141f8da9ffcc
Deadpo_Ol <https://github.com/devoind>
2022-11-15 00:23:54 +0300

    last_jobs2

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -25,9 +25,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class AdSerializer(serializers.ModelSerializer):
-    author_first_name = serializers.CharField(source='author.first_name', read_only=True)
-    author_last_name = serializers.CharField(source='author.last_name', read_only=True)
-    phone = serializers.CharField(source='author.phone', read_only=True)
+    # author_first_name = serializers.CharField(source='author.first_name', read_only=True)
+    # author_last_name = serializers.CharField(source='author.last_name', read_only=True)
+    # phone = serializers.CharField(source='author.phone', read_only=True)
+    pk = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Ad
@@ -36,6 +37,8 @@ class AdSerializer(serializers.ModelSerializer):
             'title',
             'price',
             'author',
+            'description',
+            'image',
         )
 
 
